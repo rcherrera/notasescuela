aafe4cb465c5430590ba59d96f0166ef982c6c38
Roberto <robertio.carlos@gmail.com>
2020-06-24 15:23:37 -0600

    Create pagina.php

---
+++ pagina.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Escuela Biblica</title>
+  <meta charset="UTF-8">
+</head>
+<body>
+<h1>NotasL</h1>
+<h2>varchar</h2>
+<p>
+Consulta para notas finales
+</p>
+<h2>int</h2>
